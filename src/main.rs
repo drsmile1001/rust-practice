6899e70172feec0a812f9c6f526137eb9c3864a8
drsmile <drsmile.1001@gmail.com>
2021-02-24 17:11:50 +0800

    feat: 實踐猜數字

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,32 @@
+extern crate rand;
+use rand::Rng;
+use std::{cmp::Ordering, io};
+
 fn main() {
-    println!("Hello, world!");
-    println!("line 2");
-    let mut x = 5;
-    x += 3;
-    println!("{0}", x);
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    loop {
+        println!("Please input your guess.");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        println!("You guessed: {}", guess);
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
