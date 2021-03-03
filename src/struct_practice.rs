b53934a3c7323b599087440324cd038d3a909e52
drsmile <drsmile.1001@gmail.com>
2021-03-03 21:06:02 +0800

    feat: 加入 strcut 練習

---
+++ src/struct_practice.rs
@@ -0,0 +1,36 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
+
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    let sq = Rectangle::square(10);
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+    println!("rect1 can hold rect2: {}", rect1.can_hold(&sq));
+    println!("rect1 can hold rect3: {}", rect1.can_hold(&rect3));
+}
