a4e2cb3dfa4ef53e2da4994897209e6088daf2fa
drsmile <drsmile.1001@gmail.com>
2021-06-12 16:29:15 +0800

    feat: 泛型練習，最大數字與最大字元方法並存

--- src/generics_practice.rs
+++ src/generics_practice.rs
@@ -6,17 +6,30 @@ use core::num;
 pub(crate) fn run() -> () {
     let number_list = vec![34, 50, 25, 100, 65];
 
-    let result = largest(&number_list);
+    let result = largest_i32(&number_list);
     println!("最大數字為 {}", result);
 
-    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+    let char_list = vec!['y', 'm', 'a', 'q'];
 
-    let result = largest(&number_list);
-    println!("最大數字為 {}", result);
+    let result = largest_char(&char_list);
+    println!("最大字元為 {}", result);
 }
 
 ///查詢列表中最大的數字
-fn largest(list: &[i32]) -> &i32 {
+fn largest_i32(list: &[i32]) -> &i32 {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+///查詢列表中最大的字元
+fn largest_char(list: &[char]) -> &char {
     let mut largest = &list[0];
 
     for item in list {
