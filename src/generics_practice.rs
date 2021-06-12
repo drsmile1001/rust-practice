799a16249a48f8932153a1ac2a84b163fcef03f2
drsmile <drsmile.1001@gmail.com>
2021-06-12 16:32:28 +0800

    feat: 嘗試以泛型改寫最大的東西函式

--- src/generics_practice.rs
+++ src/generics_practice.rs
@@ -6,30 +6,17 @@ use core::num;
 pub(crate) fn run() -> () {
     let number_list = vec![34, 50, 25, 100, 65];
 
-    let result = largest_i32(&number_list);
+    let result = largest(&number_list);
     println!("最大數字為 {}", result);
 
     let char_list = vec!['y', 'm', 'a', 'q'];
 
-    let result = largest_char(&char_list);
+    let result = largest(&char_list);
     println!("最大字元為 {}", result);
 }
 
-///查詢列表中最大的數字
-fn largest_i32(list: &[i32]) -> &i32 {
-    let mut largest = &list[0];
-
-    for item in list {
-        if item > largest {
-            largest = item;
-        }
-    }
-
-    largest
-}
-
-///查詢列表中最大的字元
-fn largest_char(list: &[char]) -> &char {
+///查詢列表中最大的東西
+fn largest<T>(list: &[T]) -> &T {
     let mut largest = &list[0];
 
     for item in list {
