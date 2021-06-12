3682da3663397a5358fd7326a537341e14fa318a
drsmile <drsmile.1001@gmail.com>
2021-06-12 16:25:57 +0800

    feat: 泛型練習中加入找最大數字函數

---
+++ src/generics_practice.rs
@@ -0,0 +1,29 @@
+use core::num;
+
+/**
+泛型練習
+*/
+pub(crate) fn run() -> () {
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    println!("最大數字為 {}", result);
+
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+
+    let result = largest(&number_list);
+    println!("最大數字為 {}", result);
+}
+
+///查詢列表中最大的數字
+fn largest(list: &[i32]) -> &i32 {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+use employee_management::run;
+
 mod define_enum;
 mod employee_management;
 mod fibonacci_number;
 mod front_of_house;
+mod generics_practice;
 mod guess_number;
 mod hashmap_practice;
 mod match_coins;
@@ -10,5 +13,6 @@ mod pig_latin;
 mod recoverable_errors;
 mod string_practice;
 mod temperature_converter;
-
-fn main() {}
+fn main() {
+    generics_practice::run();
+}
