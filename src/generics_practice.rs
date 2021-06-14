5a0ac188c28d35745b44701302a30d21e71568ac
drsmile <drsmile.1001@gmail.com>
2021-06-14 16:10:54 +0800

    feat: 為特徵加入預設實作

--- src/generics_practice.rs
+++ src/generics_practice.rs
@@ -53,5 +53,72 @@ pub(crate) fn run() -> () {
     let float = Point { x: 1.0, y: 4.0 };
 
     println!("p.x = {}", interger.x());
-    println!("p distance from origin = {}", float.distance_from_origin())
+    println!("p distance from origin = {}", float.distance_from_origin());
+
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
+
+    println!(
+        "1 則新推文：{} 類別：{}",
+        tweet.summarize(),
+        tweet.category()
+    );
+
+    let news = NewsArticle {
+        headline: String::from("標題"),
+        location: String::from("地點"),
+        author: String::from("作者"),
+        content: String::from("內文"),
+    };
+
+    println!("1 則新聞：{} 類別：{}", news.summarize(), news.category());
+}
+
+///定義特徵 特徵有點像是 interface
+pub trait Summary {
+    fn summarize(&self) -> String;
+    ///有預設實作的方法
+    fn category(&self) -> String {
+        String::from("一般")
+    }
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+///為特定型別NewsArticle實踐特徵
+impl Summary for NewsArticle {
+    fn summarize(&self) -> String {
+        format!("{} {} 著 ({})", self.headline, self.author, self.location)
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+///為特定型別Tweet實踐特徵
+impl Summary for Tweet {
+    fn summarize(&self) -> String {
+        format!("{} : {}", self.username, self.content)
+    }
+
+    fn category(&self) -> String {
+        if self.reply {
+            String::from("回文")
+        } else {
+            String::from("原文")
+        }
+    }
 }
